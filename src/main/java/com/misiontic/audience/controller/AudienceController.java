95d99a6e84da9b7aa461d32ee5de9964ed8738db
brayanrodallega <thebrayan1a1@outlook.com>
2022-10-08 10:09:43 -0500

    Agregando Client

--- src/main/java/com/misiontic/audience/controller/AudienceController.java
+++ src/main/java/com/misiontic/audience/controller/AudienceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/audience")
+@RequestMapping("/api/Audience")
 public class AudienceController {
 
     @Autowired
--- src/main/java/com/misiontic/audience/entities/Category.java
+++ src/main/java/com/misiontic/audience/entities/Category.java
@@ -14,6 +14,7 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String description;
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "category")
     @JsonIgnoreProperties("category")
